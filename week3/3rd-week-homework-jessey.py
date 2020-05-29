6b2a55b834c0c4acf3f0b5b49fb7459529bdb281
jessey-bee <65078641+jessey-bee@users.noreply.github.com>
2020-05-30 00:04:09 +0900

    Update 3rd-week-homework-jessey.py

--- week3/3rd-week-homework-jessey.py
+++ week3/3rd-week-homework-jessey.py
@@ -12,7 +12,7 @@ for song in songs:
     a_title_tag = song.select_one('td.info > a.title.ellipsis')
     a_artist_tag = song.select_one('td.info > a.artist.ellipsis')
     if a_title_tag is not None:
-        rank = song.select_one('td.number').text[:2].strip()
+        rank = song.select_one('td.number').text.split()
         title = a_title_tag.text.strip()
         artist = a_artist_tag.text.strip()
-        print(rank, title, artist)
+        print(rank[0], title, artist)
