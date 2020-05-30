a6f461c5f9d5b968b1c01f99f37955c4317a3ac5
zoeroh <mrkhjroh@gmail.com>
2020-05-30 17:39:20 +0900

    hw

---
+++ 8th-3w-asignment-hj.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1', headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+##################
+
+musiclist = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for music in musiclist:
+    rank = music.select_one('td.number').contents[0].strip()
+    title = music.select_one('td.info > a.title.ellipsis').text.strip()
+    musician = music.select_one('td.info > a.artist.ellipsis').text
+    print(rank, title, musician)
